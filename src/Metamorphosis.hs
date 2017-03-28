4e3f181d06c0a88f119234a950a15519bf2bb481
Maxime Bourget <bmx007@gmail.com>
2017-03-28 21:15:27 +0100

    Tranform field (name and types) works.

--- src/Metamorphosis.hs
+++ src/Metamorphosis.hs
@@ -2,7 +2,14 @@
 module Metamorphosis where
 
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import Data.Maybe
+import Control.Monad
+import Data.Char
 
+type TName = String -- Type name
+type CName = String -- Constructor name
+type FName = String -- field name
 -- | map
 
 reshape :: ((String,  [String]) -> Maybe (String, [String]) ) -> Name -> String -> Q [Dec]
@@ -14,9 +21,12 @@ reshape f recName new  = do
            (DataD context newName vars kind (map renameCons constructors) derivings )
         newName = mkName new
 
-        renameCons (NormalC name bang) = NormalC (rename name) bang
-        renameCons (RecC name bangs) = RecC (rename name) bangs
-        renameCons (InfixC bang name bang') = InfixC bang (rename name) bang'
+        reshapeBangs = mapMaybe (reshapeBang f)
+        reshapeVarBangs = mapMaybe (reshapeVarBang f)
+
+        renameCons (NormalC name bangs) = NormalC (rename name) (reshapeBangs bangs)
+        renameCons (RecC name bangs) = RecC (rename name) (reshapeVarBangs bangs)
+        -- renameCons (InfixC bang name bang') = InfixC  (reshapeBang bang) (rename name) (reshapeBang bang')
         renameCons (ForallC vars ctx con) = ForallC vars ctx (renameCons con)
         rendameCons (GadtC names vars typ) = GadtC (map rename names) vars typ
         rendameCons (RecGadtC names vars typ) = RecGadtC (map rename names) vars typ
@@ -28,6 +38,39 @@ reshape f recName new  = do
                           Just (new, _) -> new
             
       
+reshapeBang :: ((String,  [String]) -> Maybe (String, [String]) ) -> BangType -> Maybe BangType
+reshapeBang  f bang = Just bang
+
+reshapeVarBang :: ((String,  [String]) -> Maybe (String, [String]) ) -> VarBangType -> Maybe VarBangType
+reshapeVarBang  f (name, bang, typ) =
+  let base = nameBase name
+  in case f (base, typeToChain typ) of
+          Nothing -> Nothing
+          Just (new, types) -> Just ( mkName new
+                                    , bang
+                                    , chainToType types
+                                    )
+     
+
+typeToChain :: Type -> [String]
+typeToChain (ForallT _ _ typ ) = typeToChain typ
+typeToChain (AppT f t) = typeToChain f ++ typeToChain t
+typeToChain (SigT t _ ) = typeToChain t
+typeToChain (VarT n) = [nameBase n]
+typeToChain (ConT t) = [nameBase t]
+typeToChain (PromotedT t) = [nameBase t]
+typeToChain t = error $ "typeToChain not implemented for :" ++ show t
+             
+chainToType :: [String] -> Type
+chainToType [] = error "shouldn't not happen"
+chainToType [t] | isVar t = VarT (mkName t)
+                | otherwise = ConT (mkName t)
+chainToType (t:ts) =  AppT (chainToType [t]) (chainToType ts)
+  
+
+isVar [] = False
+isVar (c:cs) = isLower c
+
 inject = error "FIX ME"
 
 extract = error "FIX ME"
--- test.hs
+++ test.hs
@@ -5,7 +5,13 @@ import Metamorphosis
 data A = A
 data F a b = F a b
 data B = B Int
-data R = R { name :: String, price :: Double}
+data R = R { name :: String, price :: Double} deriving (Eq, Ord, Read, Show)
 $(reshape (const Nothing) ''A "A0")
 $(reshape (\(n, v) -> Just ("Blob" ++ n, v)) ''B "BLOB")
 $(reshape (\(n, v) -> Just (n ++ "Z", v)) ''R "Rob")
+-- Change all types to Int
+$(reshape (\(n, v) -> Just (n ++ "Int",[ "Int"])) ''R "RobInt")
+-- Add Maybe 
+$(reshape (\(n, v) -> Just (n ++ "M",  "Maybe": v)) ''R "Mob")
+-- Remove type constructors
+$(reshape (\(n, v) -> Just (n ++ "U",  [last v])) ''Mob "Uob")
