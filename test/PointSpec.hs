4c533515a9b702c4a6fdeafbd7608bf0c5542fff
Maxime Bourget <bmx007@gmail.com>
2017-04-09 11:45:55 +0100

    Adding PointSpec
    Files to test manual implementation of Prism between PointM and Point3
    (see reddit question https://redd.it/647o1l)

---
+++ test/PointSpec.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+module PointSpec where
+
+import Control.Lens
+import Test.Hspec
+
+data Point2 = Point2 { x :: Double, y :: Double } deriving (Show, Read, Eq)
+data Point3 = Point3 { x :: Double, y :: Double, z :: Double } deriving (Show, Read, Eq)
+data PointM = PointM { x :: Double,  y :: Double, z :: Maybe Double} deriving (Show, Read, Eq)
+
+point2 :: Prism' PointM Point2
+point2 = undefined
+point3 :: Prism' PointM Point3
+point3 = undefined
+
+pm2 = PointM 1 2 Nothing
+pm3 = PointM 1 2 (Just 3)
+p2 = Point2 1 2
+p3 = Point3 1 2 3
+
+spec :: Spec
+spec = do
+  describe "PointM to Point3" $ do
+    it "extract if possible" $ do
+        pm3 ^? point3 `shouldBe` (Just p3)
+    it "returns nothing if not" $ do
+        pm2 ^? point3 `shouldBe` Nothing
+    it "contructs" $ do
+      point3 # p3 `shouldBe` pm3
+    it "updates if possible" $ do
+      (point3 %~ (\(Point3 x y z) -> Point3 x y 10) $ pm3) `shouldBe` PointM 1 2 (Just 10)
+    it "doesn't updates if not" $ do
+      (point3 %~ (\(Point3 x y z) -> Point3 x y 10) $ pm2) `shouldBe` PointM 1 2 Nothing
+  describe "PointM to Point2"$ do
+    it "extract if possible" $ do
+        pm2 ^?! point2 `shouldBe` p2
+  describe "Point3 to PointM" $ do
+    it "get pointM" $ do
+      p3 ^. re point3 `shouldBe` pm3
+    -- it "set pointM if possible" $ do
+      -- (re point3 .~ (PointM 10 20 (Just 50)) $ p3) `shouldBe` (Point3 10 20 50)
+      -- (re point3 %~ _ $ p3) `shouldBe` (Point3 10 20 50)
+    -- it "set pointM" $ do
+      -- (re point3 .~ (PointM 10 20 Nothing) $ p3) `shouldBe` (Point3 1 2 3)
+    
