6a49f4de069986708a7c2c8d754c54035f4fdf2e
Maxime Bourget <bmx007@gmail.com>
2017-04-11 21:48:56 +0100

    @WIP

---
+++ src/Metamorphosis/TH.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell #-}
+module Metamorphosis.TH where
+import           Language.Haskell.TH
+import           Language.Haskell.TH.Syntax
+import           Data.List (sort, nub, group, groupBy, intercalate)
+import           Data.Function (on)
+
+import Metamorphosis.Types
+
+
+-- | Retrieves the field descriptions of data type (from its Info)
+collectFields :: Info -> [FieldDesc]
+collectFields (TyConI (DataD cxt tName vars kind cons cxt')) = concatMap go cons where
+  go (NormalC cName []) =  [ FieldDesc { _fdTypeName = nameBase tName
+                                     , _fdConsName = nameBase cName
+                                     , _fdFieldName = Nothing
+                                     , _fdPos = 0
+                                     , _fdBang = (Bang NoSourceUnpackedness NoSourceStrictness)
+                                     , _fdTypes = []
+                                     , _fdModuleName = nameModule tName
+                                     }
+                           ]
+  go (NormalC cName bangs) =  zipWith (go' cName) bangs [1..]
+  go (RecC cName varbangs) =  zipWith (go'' cName) varbangs [1..]
+  go (InfixC bang cName bang') = [ go' cName bang 1
+                                 , go' cName bang' 2
+                                 ]
+
+  go' cName (bang, typ) pos = FieldDesc { _fdTypeName = nameBase tName
+                                    , _fdConsName = nameBase cName
+                                    , _fdFieldName = Nothing
+                                    , _fdPos  = pos
+                                    , _fdBang = bang
+                                    , _fdTypes = typeToTypeNames typ
+                                    , _fdModuleName = nameModule tName
+                                    }
+
+  go'' cName (fName, bang, typ) pos = (go' cName (bang, typ) pos) {_fdFieldName = Just (nameBase fName)}
+collectFields info = error $ "collectFields only works with type declaration." ++ show ( ppr info )
+
+-- | Structure Field Descriptions to types
+fieldsToTypes :: [FieldDesc] -> [TypeDesc]
+fieldsToTypes fds = let
+  sorted = sort fds -- by type, constructor, pos
+  groups = groupBy ((==) `on` _fdType) sorted
+  mkType fields@(fd:_) = TypeDesc (_fdTypeName fd)
+                                 (_fdModuleName fd)
+                                 (fieldsToConss fields)
+  in map mkType groups
+
+
+fieldsToConss :: [FieldDesc] -> [ConsDesc]
+fieldsToConss fds = let
+  sorted = sort fds -- by type, constructor, pos
+  groups = groupBy ((==) `on` _fdConsName) sorted
+  mkCons fields@(fd:_) = ConsDesc (_fdConsName fd)
+                                 (sort fields)
+  in map mkCons groups
+
+-- | Converts a type (AST) to a list of String (chain)
+-- ex: Maybe Int -> ["Maybe", "Int"]
+typeToTypeNames :: Type -> [String]
+typeToTypeNames (ForallT _ _ typ ) = typeToTypeNames typ
+typeToTypeNames (AppT f t) = typeToTypeNames f ++ typeToTypeNames t
+typeToTypeNames (SigT t _ ) = typeToTypeNames t
+typeToTypeNames (VarT n) = [nameBase n]
+typeToTypeNames (ConT t) = [nameBase t]
+typeToTypeNames (PromotedT t) = [nameBase t]
+typeToTypeNames (ListT) = ["[]"]
+typeToTypeNames info = error $ "typeToTypeNames not implemented for :" ++ show info
+             
--- src/Metamorphosis/Types.hs
+++ src/Metamorphosis/Types.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Metarmorphosis.Types where
+module Metamorphosis.Types where
 
+import Control.Applicative
+import Control.Monad
+import Language.Haskell.TH.Syntax
 import Lens.Micro
 import Lens.Micro.TH
-import Control.Monad
 
 type ModuleName = String
 type TypeName = String
@@ -19,7 +21,7 @@ data FieldDesc = FieldDesc
   , _fdFieldName :: Maybe String -- ^ Field Name if Recorder
   , _fdBang :: Bang
   , _fdTypes :: [String] -- ^ [] means no field at all (ex Enum)
-  , _fdMName :: Maybe String -- ^ Module name . only used to use proper constructor name
+  , _fdModuleName :: Maybe String -- ^ Module name . only used to use proper constructor name
 
   } deriving (Show, Eq, Ord)
 
@@ -28,27 +30,31 @@ makeLenses ''FieldDesc
 -- | Traversal to set both name at the same time
 fdTConsName  :: Traversal' FieldDesc String
 fdTConsName f fd = let
-  fConsName = f (fdConsName fd)
-  fTypeName = f (fdTypeName fd)
-  in (\tname cname -> fd { _fdTname = tname,  _fdConsName = cname }  ) <*> fConsName fTypeName
+  fConsName = f (_fdConsName fd)
+  fTypeName = f (_fdTypeName fd)
+  in (\tname cname -> fd { _fdTypeName = tname,  _fdConsName = cname }  ) <$> fConsName <*> fTypeName
 
 
 -- | Key to identify uniquely a Field
-_fdKey :: FieldDesc -> (Maybe ModuleName, ConsName, Either Pos FieldName)
-_fdKey field = ( _fdMName fd
+_fdKey :: FieldDesc -> (Maybe ModuleName, ConsName, Either Int FieldName)
+_fdKey fd = ( _fdModuleName fd
                , _fdConsName fd
                , maybe (Left (_fdPos fd)) Right (_fdFieldName fd)
                )
 
+_fdType :: FieldDesc -> (Maybe ModuleName, TypeName)
+_fdType = liftA2 (,) _fdModuleName _fdTypeName
 -- | Denormalized description of a Type
 data TypeDesc = TypeDesc { _tdName :: TypeName
-                         , _tdCons :: [ConsDesc]
                          , _tdModuleName :: Maybe ModuleName
+                         , _tdCons :: [ConsDesc]
                          } deriving (Show, Eq, Ord)
 
 -- Denormalized description of a data constructor.
 -- All field are supposed to have their type an cons field set correctly
 data  ConsDesc = ConsDesc { _cdName :: ConsName
-                          , _cdFields :: [Fielddesc]
+                          , _cdFields :: [FieldDesc]
                           } deriving (Show, Eq, Ord)
 
+makeLenses ''TypeDesc
+makeLenses ''ConsDesc
