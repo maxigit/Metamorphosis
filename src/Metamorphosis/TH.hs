934ff94f44f6738e768f68da2c5f1ff4ccd7e316
Maxime Bourget <bmx007@gmail.com>
2017-04-20 12:05:03 +0100

    Generate constructor body. Need specs

--- src/Metamorphosis/TH.hs
+++ src/Metamorphosis/TH.hs
@@ -4,6 +4,9 @@ import           Language.Haskell.TH
 import           Language.Haskell.TH.Syntax
 
 import Metamorphosis.Internal
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Lens.Micro
 
 
 -- | Retrieves the field descriptions of data type (from its Info)
@@ -55,3 +58,56 @@ typeToTypeNames (PromotedT t) = [nameBase t]
 typeToTypeNames (ListT) = ["[]"]
 typeToTypeNames info = error $ "typeToTypeNames not implemented for :" ++ show info
              
+
+-- * Generating Code
+-- | Generates a pattern from a constructor
+-- ex: data A = A Int String
+-- >> a@(A a1 a2)
+consToPat :: (Map FieldDescPlus Name) -> ConsDesc -> Pat
+consToPat fieldToName consDesc = let
+  pats = map  fieldName (consDesc ^. cdFields)
+  fieldName field = maybe WildP VarP (Map.lookup field fieldToName)
+  in ConP (mkName $ consDesc ^. cdName) pats
+
+  
+  
+
+-- | Generates a tuple patter from a list of constructors
+-- ex: [(A Int), (B String)] -> (A a, B b)
+conssToTuplePat :: (Map FieldDescPlus Name) -> [ConsDesc] -> Pat
+conssToTuplePat fieldToName consDescs = let
+  in TupP (map (consToPat fieldToName) consDescs )
+
+-- | Generates a constructor clause of the form
+-- (A a) (XY x y) = (f AXZ  (g a) (g x), f Y (g y)
+genConsClause :: [[ConsDesc]] -> [ConsDesc] -> Q Clause
+genConsClause sources targets = do
+  fieldToName <- genNameMap  (sources ^. each . each . cdFields )
+  let pats = map (conssToTuplePat fieldToName) sources
+  let body = TupE (map (genConsBodyE  fieldToName) targets)
+  return $  Clause pats (NormalB body) []
+
+
+
+-- | Generates an map of uncapturable pattern (argument name)
+-- from a list of constructors
+genNameMap :: [FieldDescPlus] -> Q (Map FieldDescPlus Name)
+genNameMap fields = do
+    pats <- mapM (genName) fields 
+    return $ Map.fromList (zip fields pats)
+
+
+genName :: FieldDescPlus -> Q Name
+genName fp = newName $ case (fp ^. fpField . fdFieldName) of
+    Nothing -> fp ^. fpCons . cdName ++ show (fp ^. fpField . fdPos)
+    Just name -> name
+
+genConsBodyE :: Map FieldDescPlus Name -> ConsDesc -> Exp
+genConsBodyE fieldToName consDesc = let
+  cons =  ConE (mkName $ consDesc ^. cdName)
+  fieldEs = map fieldToE (consDesc ^. cdFields)
+  fieldToE fp = maybe (TupE []) VarE (Map.lookup fp fieldToName)
+  in foldl AppE cons fieldEs
+
+
+
