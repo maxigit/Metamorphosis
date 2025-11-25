27ead06b513140faa7fa6aa3b27c87cf140237ba
Maxime Bourget <bmx007@gmail.com>
2025-11-25 13:48:25 +0000

    Fix tests

--- test/THSpec.hs
+++ test/THSpec.hs
@@ -91,40 +91,40 @@ spec = do
     let genCopy = genConsClause identityBCR
 
     it "generates copy for simple constructor" $ do
-      genCopy [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = (A (a1_0) (a2_1))"
+      genCopy [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = A (a1_0) (a2_1)"
 
     it "generates copy for record constructor" $ do
-      genCopy [[aRecord]] [aRecord'] `shouldLookQ` "(Record style_0 price_1) = (Record (style_0) (price_1))"
+      genCopy [[aRecord]] [aRecord'] `shouldLookQ` "(Record style_0 price_1) = Record (style_0) (price_1)"
     it "generates copy for of tuples" $ do
       genCopy [[aRecord, pPoint]] [aRecord', pPoint']
         `shouldLookQ` "(Record style_0 price_1, Point x_2 y_3) = (Record (style_0) (price_1), Point (x_2) (y_3))"
     it "generates wildcard for non used pattern" $ do
-      genCopy [[aRecord]] [pPoint] `shouldLookQ` "(Record _ _) = (Point () ())"
+      genCopy [[aRecord]] [pPoint] `shouldLookQ` "(Record _ _) = Point () ()"
 
     it "generates applicatives" $ do
-      genConsClause applicativeBCR [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = (A <$> (convertA a1_0) <*> (convertA a2_1))"
+      genConsClause applicativeBCR [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = A <$> (convertA a1_0) <*> (convertA a2_1)"
 
     it "generates to monoid" $ do
-      genConsClause (monoidBCR "return") [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = (mempty <> (return a1_0) <> (return a2_1))"
+      genConsClause (monoidBCR "return") [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = mempty <> (return a1_0) <> (return a2_1)"
 
     it "generates to monoid with Pure" $ do
-      genConsClause (monoidPureBCR "show") [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = (mempty <> ((pure . show) a1_0) <> ((pure . show) a2_1))"
+      genConsClause (monoidPureBCR "show") [[aA]] [aA'] `shouldLookQ` "(A a1_0 a2_1) = mempty <> ((pure . show) a1_0) <> ((pure . show) a2_1)"
 
     it "use multiple constructors (tuples)" $ do
        let f = ( return . (fdFieldName .~ (Just "P")) . (fdTConsName .~ "P"))
            [p] = applyFieldMapping f [point]
-       genConsClause identityBCR [[pPoint]] (p ^. tdCons) `shouldLookQ` "(Point x_0 y_1) = (P (GHC.Tuple.(,) x_0 y_1))"
+       genConsClause identityBCR [[pPoint]] (p ^. tdCons) `shouldLookQ` "(Point x_0 y_1) = P (GHC.Tuple.(,) x_0 y_1)"
 
   describe "genConversion" $ do
 
     it "generates simple converter" $ do
       genConversion "PointToPoint" identityBCR [[point]] [point'] `shouldLookQ`
-        "iPointToPoint (Point x_0 y_1) = (Point (x_0) (y_1))"
+        "iPointToPoint (Point x_0 y_1) = Point (x_0) (y_1)"
 
     it "uses correct constructor for sum types" $ do
       genConversion "AToA" identityBCR [[a]] [a'] `shouldLookQ`
-        "iAToA (A a1_0 a2_1) = (A (a1_0) (a2_1)) \
-        \iAToA (Record style_2 price_3) = (Record (style_2) (price_3))"
+        "iAToA (A a1_0 a2_1) = A (a1_0) (a2_1) \
+        \iAToA (Record style_2 price_3) = Record (style_2) (price_3)"
 
   describe "generateType" $ do
     it "generates complex type" $ do
@@ -133,19 +133,19 @@ spec = do
 
     it "generates parametric type" $ do
       generateType [] point `shouldLook`
-        "data Point a = Point {x :: a, y :: a}" 
+        "data Point a = Point {x :: a, y :: a} deriving" 
 
   describe "genZip" $ do
     it "generate a zip between the fields of two identical data" $ do
       genZip 2 "zipPoint" "f" point Nothing `shouldLookQ`
-        "zipPoint (Point x_0 y_1) (Point x_2 y_3) = (Point (f x_0 x_2) (f y_1 y_3))"
+        "zipPoint (Point x_0 y_1) (Point x_2 y_3) = Point (f x_0 x_2) (f y_1 y_3)"
     it "generate a zip with given constructor " $ do
       genZip 2 "zipPoint" "f" point (Just ''A) `shouldLookQ`
-        "zipPoint (Point x_0 y_1) (Point x_2 y_3) = (THSpec.A (f x_0 x_2) (f y_1 y_3))"
+        "zipPoint (Point x_0 y_1) (Point x_2 y_3) = THSpec.A (f x_0 x_2) (f y_1 y_3)"
     it "generate a zip between the fields of n identical data" $ do
       genZip 3 "zipPoint" "f" point Nothing `shouldLookQ`
         "zipPoint (Point x_0 y_1) (Point x_2 y_3) (Point x_4 y_5)\
-        \ = (Point (f x_0 x_2 x_4) (f y_1 y_3 y_5))"
+        \ = Point (f x_0 x_2 x_4) (f y_1 y_3 y_5)"
 
        
 
